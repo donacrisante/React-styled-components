36a873a7613694c9e847cdf8ed12ee759a598612
Donatella Crisante <dona.crisante@zoho.com>
2023-06-15 12:52:57 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+import styled from "styled-components";
 
 export default function HomePage() {
   return (
     <div>
+      <Align>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents isBlack />
+      </Align>
     </div>
   );
 }
+
+const Align = styled.div `
+display: flex;
+flex-direction: row;
+`;
+
+
+
